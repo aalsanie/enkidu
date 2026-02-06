c9f8a630c7693a9e49dba16f25cc58b7f2251ef8
aalsanie <ahmad.alsanie@hotmail.com>
2026-02-06 07:59:47 +0300

    build: restore Kotlin stdlib globally; exclude stdlib only for IntelliJ plugin to avoid conflicts

--- enkidu-intellij-plugin/build.gradle.kts
+++ enkidu-intellij-plugin/build.gradle.kts
@@ -45,6 +45,14 @@ dependencies {
     }
 }
 
+
+// IntelliJ Platform bundles Kotlin; avoid classpath conflicts by excluding Kotlin stdlib from the plugin artifact.
+configurations.configureEach {
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+}
+
 intellijPlatform {
     pluginConfiguration {
         name = "Enkidu Linkage Doctor"
